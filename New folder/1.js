d7caab1a2db68bc6f4ae219eb442b5a6dcda9126
Jivanshu-Mittal <jivanshumitt1052@gmail.com>
2025-10-07 14:19:43 +0530

    learning continues

---
+++ New folder/1.js
@@ -0,0 +1,8 @@
+<script>
+    function append(num){document.getElementById('result').value +=
+        num};
+    function clearResult(){document.getElementById('result').value =
+        ''};
+    function calculate(){document.getElementById('result').value =
+        eval(document.getElementById('result').value)};
+</script>
