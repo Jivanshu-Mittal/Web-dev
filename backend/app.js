4dcc4867add101e7fbe3379cf5fc74a26eba66ee
Jivanshu-Mittal <jivanshumitt1052@gmail.com>
2026-01-17 22:25:26 +0530

    Add initial backend app.js and index.html files
    Introduces a basic backend setup with app.js containing commented timer examples and a Promise, and index.html loading the script. This serves as a starting point for further backend development.

---
+++ backend/app.js
@@ -0,0 +1,23 @@
+// setTimeout(() => {
+//     console.log("DB operation");
+// }, 3000)
+
+// let interval = setInterval(() => {
+//     console.log("Interval");
+// }, 2000);
+
+// let interval1 = setInterval(() => {
+//     console.log("hello");
+// }, 2000);
+
+// setTimeout(() => {
+//     clearInterval(interval1);
+// }, 10000)
+
+
+let p1 = new Promise((resolve, reject) => {
+    // reject("promise completed")
+})
+
+console.log(p1);
+async function fetchPost
